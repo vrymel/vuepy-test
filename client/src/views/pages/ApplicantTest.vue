3b067eac487878794e3db45163c1791c37e2048b
Vrymel Omandam <vrymel.dev@gmail.com>
2020-09-23 22:37:43 +0800

    Group country by first letters

--- client/src/views/pages/ApplicantTest.vue
+++ client/src/views/pages/ApplicantTest.vue
@@ -20,22 +20,29 @@
 
     <div class="mt-5">
       <p>Select Banned Countries</p>
-      <CTabs variant="pills" :active-tab="0">
-        <CTab title="ABC">
-          <CInputCheckbox
-              v-for="country in countryList"
-              :key="country.code"
-              :label="country.name"
-              :checked="selectedCountriesMap[country.code]"
-              @update:checked="addCountry(country, $event)"/>
-          <CInputCheckbox label="Philippines" @update:checked="addCountry('Philippines', $event)"/>
+      <CTabs variant="pills" :active-tab="0" @update:activeTab="selectCountryGroup">
+        <CTab v-for="groupedCountry in countriesByFirstLetters"
+              :key="groupedCountry.name"
+              :title="groupedCountry.name">
         </CTab>
       </CTabs>
+
+      <div>
+        <CInputCheckbox
+            v-for="country in displayCountryList"
+            :key="country.code"
+            :label="country.name"
+            :checked="selectedCountriesMap[country.code]"
+            @update:checked="addCountry(country, $event)">
+          {{country}}
+        </CInputCheckbox>
+      </div>
     </div>
   </CContainer>
 </template>
 
 <script>
+import chunk from 'lodash/chunk';
 // todo: move to backend
 import countriesJson from './countries.json';
 
@@ -45,7 +52,46 @@ export default {
     return {
       selectedCountries: [],
       selectedCountriesMap: {},
-      countryList: countriesJson
+      countryList: countriesJson,
+      selectedCountryGroup: { name: 'ABC', data: ['A', 'B', 'C'] }
+    }
+  },
+  computed: {
+    countriesByFirstLetters: function () {
+      const uniqueFirstHalf = new Set();
+      for (let c of this.countryList) {
+        let first = c.name[0];
+
+        // Manually add MN later
+        if (first !== 'M' && first !== 'N')
+          uniqueFirstHalf.add(first);
+
+        // No X in countryList so manually add it here
+        if (uniqueFirstHalf.has('W'))
+          uniqueFirstHalf.add('X');
+      }
+
+      const chunked = chunk(Array.from(uniqueFirstHalf), 3);
+      chunked.splice(4, 0, ['M', 'N']);
+
+      return chunked.map((chunk) => {
+        return {
+          'name': chunk.join(''),
+          'data': chunk
+        }
+      })
+    },
+    displayCountryList: function() {
+      const allowedFirstLetters = new Set(this.selectedCountryGroup.data);
+      const displayCountries = [];
+
+      for (let c of this.countryList) {
+        if (allowedFirstLetters.has(c.name[0])) {
+          displayCountries.push(c);
+        }
+      }
+
+      return displayCountries;
     }
   },
   methods: {
@@ -61,6 +107,9 @@ export default {
     removeCountry(country) {
       this.selectedCountries = this.selectedCountries.filter((c) => c !== country)
       delete this.selectedCountriesMap[country.code];
+    },
+    selectCountryGroup(selectedTabIndex) {
+      this.selectedCountryGroup = this.countriesByFirstLetters[selectedTabIndex];
     }
   }
 }
