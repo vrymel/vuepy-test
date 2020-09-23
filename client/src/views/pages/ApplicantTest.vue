6d3ddc3c3fa708193f4b8f54784698d7226acb52
Vrymel Omandam <vrymel.dev@gmail.com>
2020-09-23 21:43:03 +0800

    Added state and event handler

--- client/src/views/pages/ApplicantTest.vue
+++ client/src/views/pages/ApplicantTest.vue
@@ -3,22 +3,24 @@
     <div>
       <h3>Banned</h3>
       <div>
-        <CButton block color="info">Canada</CButton>
-        <CButton block color="info">Germany</CButton>
+        <CButton
+            v-for="country in selectedCountries"
+            :key="country"
+            block
+            color="info">
+          {{ country }}
+        </CButton>
       </div>
       <span>Select Banned Countries</span>
     </div>
 
     <div>
       <CTabs variant="pills" :active-tab="1">
-        <CTab title="Home">
-          1. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore
-          et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
-          dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
-          officia deserunt mollit anim id est laborum.
+        <CTab title="ABC">
+          <CButton block color="info" @click="addCountry('Germany')">Germany</CButton>
+          <CButton block color="info" @click="addCountry('Philippines')">Philippines</CButton>
         </CTab>
-        <CTab title="Profile">
+        <CTab title="DEF">
           2. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore
           et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
           aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
@@ -32,6 +34,16 @@
 
 <script>
 export default {
-  name: 'ApplicantTest'
+  name: 'ApplicantTest',
+  data() {
+    return {
+      selectedCountries: []
+    }
+  },
+  methods: {
+    addCountry(country) {
+      this.selectedCountries.push(country);
+    }
+  }
 }
 </script>
