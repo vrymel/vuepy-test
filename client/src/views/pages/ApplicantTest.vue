acaf528ecc9bcd498fa208dc520d06c96bccb10e
Vrymel Omandam <vrymel.dev@gmail.com>
2020-09-23 22:05:06 +0800

    Added countries json

--- client/src/views/pages/ApplicantTest.vue
+++ client/src/views/pages/ApplicantTest.vue
@@ -7,10 +7,10 @@
       <div class="d-flex">
         <CButton
             v-for="country in selectedCountries"
-            :key="country"
+            :key="country.code"
             class="mr-2"
             color="info">
-          {{ country }}
+          {{ country.name }}
         </CButton>
       </div>
     </div>
@@ -19,7 +19,11 @@
       <p>Select Banned Countries</p>
       <CTabs variant="pills" :active-tab="0">
         <CTab title="ABC">
-          <CInputCheckbox label="Germany" @update:checked="addCountry('Germany', $event)"/>
+          <CInputCheckbox
+              v-for="country in countryList"
+              :key="country.code"
+              :label="country.name"
+              @update:checked="addCountry(country, $event)"/>
           <CInputCheckbox label="Philippines" @update:checked="addCountry('Philippines', $event)"/>
         </CTab>
       </CTabs>
@@ -28,11 +32,18 @@
 </template>
 
 <script>
+// todo: move to backend
+import countriesJson from './countries.json';
+
 export default {
   name: 'ApplicantTest',
+  mounted() {
+    console.log(countriesjson);
+  },
   data() {
     return {
-      selectedCountries: []
+      selectedCountries: [],
+      countryList: countriesJson
     }
   },
   methods: {
